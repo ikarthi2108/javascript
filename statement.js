d13392a5961b51bde8e938c1a643e12b5adbf7d4
ikarthi2108 <karthicloud6@gmailcom>
2023-02-22 16:15:24 +0530

    third commit

---
+++ statement.js
@@ -0,0 +1,22 @@
+function gender(){
+    var gender=document.getElementById('gen').value;
+   /*  if(gender=="male"){
+        document.write("He is male");
+     }
+     else if(gender=="female"){
+        document.write("She is female");
+     }
+     else {
+        document.write("invalid input")
+     }*/
+    switch(gender){
+        case 'male' :
+            document.write("male");
+            break;
+        case 'female':
+            document.write("Female");
+            break;      
+        default:
+            document.write("invalid choice");
+    }
+}
